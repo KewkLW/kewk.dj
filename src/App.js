63c503aef7e4c9b136fccfabd46fb3cd7bd4be19
KewkLW <57611539+KewkLW@users.noreply.github.com>
2022-07-10 05:52:18 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/o-8FHXpYZTm8Jou2/scene.splinecode" />
+  );
+}
